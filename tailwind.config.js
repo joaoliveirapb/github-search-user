c5022bea1d1d4bda0841dbce6fb80ac072056a16
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-06-04 15:33:17 -0300

    project initialized and header done

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        blue: {
+          500: '#0079FF',
+          600: '#4B6A9B',
+          700: '#1E2A47',
+          950: '#141D2F',
+        },
       },
     },
   },
